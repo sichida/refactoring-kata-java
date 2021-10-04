5036d335b8fb04085eb3782407517efd16ad163f
Shoun ICHIDA <shoun.ichida@store1.fr>
2021-10-04 11:46:09 +0200

    Introducing lombok

--- src/main/java/com/sipios/refactoring/dto/BodyDto.java
+++ src/main/java/com/sipios/refactoring/dto/BodyDto.java
@@ -1,30 +1,16 @@
 package com.sipios.refactoring.dto;
 
-public class BodyDto {
+import lombok.Builder;
+import lombok.Data;
 
+@Data
+@Builder
+public class BodyDto {
     private ItemDto[] items;
     private String type;
 
     public BodyDto(ItemDto[] items, String type) {
         this.items = items;
-        this.type = type;
-    }
-
-    public BodyDto() {}
-
-    public ItemDto[] getItems() {
-        return items;
-    }
-
-    public void setItems(ItemDto[] items) {
-        this.items = items;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
         this.type = type;
     }
 }
--- src/main/java/com/sipios/refactoring/dto/ItemDto.java
+++ src/main/java/com/sipios/refactoring/dto/ItemDto.java
@@ -1,33 +1,18 @@
 package com.sipios.refactoring.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
+import lombok.Data;
 
+@Data
+@Builder
 public class ItemDto {
-
     private String type;
     @JsonProperty("nb")
     private int quantity;
 
-    public ItemDto() {}
-
     public ItemDto(String type, int quantity) {
         this.type = type;
-        this.quantity = quantity;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public int getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(int quantity) {
         this.quantity = quantity;
     }
 }
